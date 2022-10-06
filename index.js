65a6a53b48d4ea83fcc7c0b48aa69fe17f0b79ce
Rico <grico484@gmail.com>
2022-10-06 17:51:50 -0400

    finished project

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+let toggle = document.getElementById('toggle');
 let botaoMenu = document.querySelector(".menu-hamburger");
 let menuLateral = document.querySelector(".nav-bar-list");
+
+toggle.onclick = function(){
+    toggle.classList.toggle('active');
+}
 
 botaoMenu.addEventListener('click', ()=> {
     menuLateral.classList.toggle('nav-bar-list-ativo');
